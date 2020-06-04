0386f6742b17855afa86d8afb5ea83ef99b1cbb1
John Rodriguez <roamingstimuli@gmail.com>
2020-06-03 20:49:37 -0400

    initial commit

---
+++ desktop/src/com/mygdx/loderunner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.loderunner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.loderunner.LodeRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LodeRunner(), config);
+	}
+}
